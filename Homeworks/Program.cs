2809471aad60174a43f7b49c7b285cdcd5c2bace
Sezer <Sezer@SEZER-THINKPAD>
2021-02-12 22:27:30 +0300

    Add project files.

---
+++ Homeworks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homeworks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> dictionary = new Dictionary<int, string>();
+            dictionary.Add(63, "Samet Vural");
+            dictionary.Add(94, "Sezer Bozbıyık");
+            dictionary.Add(95, "Celal Seyidov");
+
+            
+
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(123, "sezer");
+
+            myDictionary.List();
+            int sonuc = myDictionary.Count;
+            Console.WriteLine(sonuc);
+        }
+    }
+}
